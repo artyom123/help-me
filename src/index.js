107aef3ab8ee2634980fe3d40ad33fd92d84e471
artyom123 <artvirsh@mail.ru>
2018-03-03 17:39:02 +0300

    decided 12

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 module.exports = function count(s, pairs) {
-    let N = 1, answer = 0, numStr;
+    let N = 1, answer = 1, numStr;
   
     for(let i = 0; i < pairs.length; i++){
         N *= Math.pow(pairs[i][0], pairs[i][1]); 
     }
-    if (N > 100000000) return 0;
-    
-    for(let k = 1; k <= N; k++){
+
+    if (s[0] === '1' && s.length === 1){
+        pairs.forEach(item=>answer *= (item[0] - 1));
+        return answer;
+    }
+        
+    if (N > 10000000) return 0;
+
+    for(let k = 0; k < N; k++){
+        let  numnP = 1;
         for(let j = 0; j < s.length; j++){
-            numStr = nod(k + j,N);
-            if(s[j] === '1' && numStr == 1) answer++;
-            if(s[j] === '0' && numStr !== 1) answer++;
+            numStr = nod(N, k + j);
+            if(s[j] === '1' && numStr !== 1) numnP = 0;
+            else if(s[j] === '0' && numStr === 1) numnP = 0;
         }
-      
+        answer += numnP;
     }
     
-      const result = (s.length >1 ? Math.floor(answer/3) : answer) % 1000000007;
-    
-    return result;
+    return (answer - 1) % 1000000007;
 }
 
 function nod(n, m) {
