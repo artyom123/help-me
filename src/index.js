d79feb9c620ebf92b4b955f54ea8c76c036c3bfd
Artyom <artvirsh@mail.ru>
2018-09-17 14:28:24 +0300

    decided 13

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 module.exports = function count(s, pairs) {
-    let N = 1, answer = 1, numStr;
+    let N = 1, answer = 1;
   
     for(let i = 0; i < pairs.length; i++){
         N *= Math.pow(pairs[i][0], pairs[i][1]); 
     }
 
-    if (s[0] === '1' && s.length === 1){
-        pairs.forEach(item=>answer *= (item[0] - 1));
+    if (N > 100000000) {
+        if(pairs.length === 1) return double(Math.pow(pairs[0][0], 9), (pairs[0][1] - 1) / 9);
+        else if(s !== '1') return 0;
+    }
+
+    if (s === '1' && s.length === 1){
+        pairs.forEach(item => answer *= (item[0] - 1));
         return answer;
     }
-        
-    if (N > 10000000) return 0;
 
-    for(let k = 0; k < N; k++){
+    for(let k = 0; k < N; k++) {
         let  numnP = 1;
-        for(let j = 0; j < s.length; j++){
-            numStr = nod(N, k + j);
-            if(s[j] === '1' && numStr !== 1) numnP = 0;
-            else if(s[j] === '0' && numStr === 1) numnP = 0;
+        for(let j = 0; j < s.length; j++) {
+            if(s[j] === '1' && nod(N, k + j) !== 1) numnP = 0;
+            else if(s[j] === '0' && nod(N, k + j) === 1) numnP = 0;
         }
         answer += numnP;
     }
@@ -33,4 +35,13 @@ function nod(n, m) {
     else { 
       return Math.abs(n);  
     }
+}
+
+function double(a, b) {
+    let s = a;
+
+    for(let y = 1; y < b; y++) {
+        s = (s * a) % 1000000007;
+    }
+    return s;
 }
